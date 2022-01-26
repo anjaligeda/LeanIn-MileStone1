54331fecfebe8d3453d032dd2538250dd0abf3bb
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2022-01-26 15:07:01 +0530

    Create arithmeticoperators.py

---
+++ arithmeticoperators.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+   '''Input--a=3
+          b=2
+   Output--5
+           1
+           6'''
