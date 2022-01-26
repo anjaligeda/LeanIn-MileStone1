fa6efcda25d37d5a83184dd4d404a9640003295e
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2022-01-26 15:12:10 +0530

    Create Division_operator.py

---
+++ Division_operator.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+    
+   ''' Input--a=4
+           b=3
+      Output--1
+              1.333333333'''
