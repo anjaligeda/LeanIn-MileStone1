8657f7238aebd83b551331efbd40b424d097b648
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2022-02-01 10:07:34 +0530

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,5 +1,13 @@
 if __name__ == '__main__':
     n = int(input())
+    #for Loop
     for i in range(n):
         x=i*i
+        print(x)
+        
+    #while Loop
+    i=0   
+    while i<n:
+        x=i*i
+        i=i+1
         print(x)
